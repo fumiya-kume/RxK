e80a718eeb19adb38486826353778baef346fc66
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-19 01:04:18 +0900

    Add onError, onCompleted

--- app/src/test/java/com/kuxu/rxk/ObservableTest.kt
+++ app/src/test/java/com/kuxu/rxk/ObservableTest.kt
@@ -2,6 +2,7 @@ package com.kuxu.rxk
 
 import org.junit.Assert
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 class ObservableTest {
     @Test
@@ -20,6 +21,45 @@ class ObservableTest {
         observable.onNext(message)
 
         Assert.assertEquals(message, onNextResult)
+
+        disposable.dispose()
+    }
+
+    @Test
+    fun CanObserverbleOnError() {
+        val observable = Observable<String>()
+
+        var onErrorResult: Throwable? = null
+        val targetException = NullPointerException()
+
+        val disposable = observable.subscribe(
+            onNext = { },
+            onError = { onErrorResult = it },
+            onCompleted = { }
+        )
+
+        observable.onError(targetException)
+
+        Assert.assertEquals(onErrorResult, targetException)
+
+        disposable.dispose()
+    }
+
+    @Test
+    fun CanOnCompletedable() {
+        val observable = Observable<String>()
+
+        val latch = CountDownLatch(1)
+
+        val disposable = observable.subscribe(
+            onNext = { },
+            onError = { },
+            onCompleted = { latch.countDown() }
+        )
+
+        observable.onCompleted()
+
+        latch.await()
 
         disposable.dispose()
     }
